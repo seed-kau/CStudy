75cadbd5f2a7b27103bf2c3132b0631bfe528b0a
JuHyang <kkss2889@naver.com>
2019-07-06 18:18:27 +0900

    1week stdio

---
+++ 1week/hello world.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+int main ()
+{
+	int a, b;
+	printf("두 숫자를 입력하세요 :");
+	scanf ("%d %d", &a, &b);
+	printf ("a + b = %d\n", a+b);
+	printf ("a - b = %d\n", a-b);
+	printf ("a * b = %d\n", a*b);
+	printf ("a / b = %d\n", a/b);
+	return 0;	
+}
+	
